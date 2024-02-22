fd78d6fd29e8838b84bea625fd6fec6d371a22b5
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-02-22 16:21:29 -0300

    feat: JWT token implementation

--- api.go
+++ api.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 )
 
@@ -35,6 +37,35 @@ func makeHTTPHandleFunc(f APIFunc) http.HandlerFunc {
 	}
 }
 
+func withJWTAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth middleware")
+		tokenStr := r.Header.Get("x-jwt-token")
+
+		token, err := validateJWT(tokenStr)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "invalid token"})
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		fmt.Println(claims)
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenStr string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected singin method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+}
+
 func getID(r *http.Request) (int, error) {
 	idStr := mux.Vars(r)["id"]
 	id, err := strconv.Atoi(idStr)
@@ -42,6 +73,18 @@ func getID(r *http.Request) (int, error) {
 		return id, fmt.Errorf(`invalid Id given '%s'`, idStr)
 	}
 	return id, nil
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
 }
 
 func NewApiServer(listenAddr string, store Storage) *APIServer {
@@ -54,7 +97,7 @@ func NewApiServer(listenAddr string, store Storage) *APIServer {
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountById))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountById)))
 	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("JSON API Server running on port:", s.listenAddr)
@@ -102,6 +145,11 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	if err := s.store.CreateAccount(account); err != nil {
 		return err
 	}
+	tokenStr, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+	fmt.Println("JET Token:", tokenStr)
 	return WriteJSON(w, http.StatusOK, account)
 }
 
--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading env file", err)
+	}
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
